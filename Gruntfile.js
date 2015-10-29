d3fa23837641edc36860c7c76814bf673f93dabc
Michael Champlin <michael.champlin@bottlerocketstudios.com>
2015-10-28 23:22:33 -0500

    Meta, links, contact, pre-flight

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function(grunt) {
       },
       build: {
         options: {
-          style: 'compressed'
+          style: 'compact'
         },
         files: {
           'build/assets/css/main.css': 'assets/css/main.sass' 
